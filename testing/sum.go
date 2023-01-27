d958e44bee3cd1aac0c2ba34bb73d86da8f781c2
Vickopiyo <opiyookoth2016@gmail.com>
2023-01-27 11:57:10 +0300

    Go testing tools and commands

--- testing/sum.go
+++ testing/sum.go
@@ -1,8 +1,30 @@
 package testing  
+    
+// CODE COVERAGE--is the way of determining  how much of code is actually covered through the tests.
+// go test -cover  - checks amount of code coverage in testfile.in this case only sum() was tested and getmax()-untested thus 25% of statement tested       
 
+// ALL GO TESTS RUN AT THE FOLDER(PACKAGE)  BEING TESTED 
 
-func sum(  a, b int) int {   
-   
+//  go test - testing a package 
+
+// go test -cover   
+
+// go test -coverprofile=coverage.out  - works same as go test -cover only that it creates a file coverage.out     
+
+// go tool cover -func=coverage.out -  shows each func that has been tested with percentage test.sum()--100%..GetMax()--0%..
+// 	Total is 33% of sum.go tested 
+
+func sum(  a, b int) int {     
 	return a + b 
-	
+	      
+}     
+   
+
+func GetMax(a, b int)  int{  
+	  
+  if a > b {
+	return a 
+  }    
+
+  return b 
 }
--- testing/sum_test.go
+++ testing/sum_test.go
@@ -2,18 +2,45 @@ package testing
 import "testing"
 
 
-func TestSum(t *testing.T)  {  
+// TEST FUNC always begin with Uppercases  
+// Test func must begin with word Test them completed with file being tested 
+// Test files always extend with _test        3
 
 
-	// Test condition from sum()---in the sum.go 
-	  total := sum(5, 5)          
-
-    //  If test fails 
-	  if total != 10 {
-		t.Errorf("Test failed, have %d, was expecting %d", total, 10)
-    
-	  }     
 
 
-}                    
+// func TestSum(t *testing.T)  {  
+// 	// Test condition from sum()---in the sum.go 
+// 	  total := sum(5, 5)          
 
+//     //  If test fails            
+// 	  if total != 10 {
+// 		t.Errorf("Test failed, have %d, was expecting %d", total, 10)         
+// 	  }            
+	  
+// }                    
+     
+func TestSum(t *testing.T)  {       
+// A slice of type struct..the structs elements(a,b,c) of type int.Each struct has 3 elemnts.Expressly initialiaze the values of the structs 
+// SLices  only tekes elements of the same  type!!!!
+    table := []struct{
+     
+		a int  
+		b int  
+		c int
+	}{
+		{1,2,3}, 
+		{2,2,4},
+		{25,25,50},
+	}  
+     // iterating through the table to check the values in each sruct        
+	 for _,  item:= range table {
+
+         total := sum(item.a, item.b)     
+		 if total != item.c{
+         
+			t.Errorf("Test failed, have %d , was expecting %d ",  item.c, total)        
+		 }
+	 }      
+     
+}        

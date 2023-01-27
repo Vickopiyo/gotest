5e7344c3169090bc8226939be72d8c687b263e69
Vickopiyo <opiyookoth2016@gmail.com>
2023-01-27 17:17:54 +0300

    Completes in Go

--- testing/sum.go
+++ testing/sum.go
@@ -5,20 +5,29 @@ package testing
 
 // ALL GO TESTS RUN AT THE FOLDER(PACKAGE)  BEING TESTED 
 
-//  go test - testing a package 
+//  go test - testing a package but reeurns in PASS or FAIL aspect only 
 
-// go test -cover   
+// go test -cover  testing package but outputs in percentage--  code coverage  
 
 // go test -coverprofile=coverage.out  - works same as go test -cover only that it creates a file coverage.out     
 
-// go tool cover -func=coverage.out -  shows each func that has been tested with percentage test.sum()--100%..GetMax()--0%..
-// 	Total is 33% of sum.go tested 
+// go tool cover -func=coverage.out -  shows each func that has been tested with percentage test.sum()--100%..GetMax()--0(before writing GO tests  for GetMax(())   
 
-func sum(  a, b int) int {     
+// 	Total is 33% of sum.go tested     
+
+// go tool cover -html=coverage.out -   run after running go tool cover -func=coverage.out to open web browser to display covered and untracked funcs        
+       
+// go test -cpuprofile=cou.out -  used checkout cpu activty while executing programs--ususally inidicates time of excution     
+
+// go tool pprof cou.out - prints info about cpu  execution of program in terms of file , time , duration..it also enters interactive mode..Once it enters interactive mode we can use command like (top-breaks down the top nodes --func which consumes the largest time ..)  ..another one is (list--  shows the functons stil)   ,,there is (web)  too  (quit--leaving the interactive mode )  
+        
+// After installing graphviz , visualization tool ...run go tool pprof cou.out-  to enter interactive mode  .. enter top and then enter list Fibonnaci,  then enter web to redirect you to a webpage..enter pdf in the interactive mode dowload a pdf to project about  cpu activity and func executed   
+
+   
+func Sum( a, b int) int {     
 	return a + b 
 	      
 }     
-   
 
 func GetMax(a, b int)  int{  
 	  
@@ -27,4 +36,15 @@ func GetMax(a, b int)  int{
   }    
 
   return b 
-}
+}                         
+ 
+//PROFILING - analyzing the complexity and costs of a go program such as its memory usage and frequently called funcs to identify the expensive sections of a go program   
+
+func Fibonacci(n int)  int{       
+	
+	if n <= 1{
+	  return n 
+	}
+   
+	return Fibonacci(n -1 ) + Fibonacci(n -2 )
+}     
--- testing/sum_test.go
+++ testing/sum_test.go
@@ -7,8 +7,6 @@ import "testing"
 // Test files always extend with _test        3
 
 
-
-
 // func TestSum(t *testing.T)  {  
 // 	// Test condition from sum()---in the sum.go 
 // 	  total := sum(5, 5)          
@@ -36,11 +34,77 @@ func TestSum(t *testing.T)  {
      // iterating through the table to check the values in each sruct        
 	 for _,  item:= range table {
 
-         total := sum(item.a, item.b)     
+         total := Sum(item.a, item.b)       
+
 		 if total != item.c{
          
-			t.Errorf("Test failed, have %d , was expecting %d ",  item.c, total)        
+			t.Errorf("Test failed, have %d , was expecting %d ",  item.c, total)            
 		 }
 	 }      
      
 }        
+   
+
+func TestGetMax(t *testing.T)  {
+	  
+	table :=[]struct{
+
+        a int 
+		b int   
+		c int
+        
+	}{
+         
+    {4, 2, 4},
+	{6, 3, 6},  
+	// Adding a  struct here with last element not matching test makes the other part of getMax()--tested return b  
+	// here 3(b) is greater than  a thus returns b unlike previously where a has always been greater.
+	// After this code average  will 100% 
+    {2, 3, 3},
+		
+	}
+  
+    for _, item := range table {  
+           
+         max := GetMax(item.a, item.b)   
+
+    if max != item.c{
+		t.Errorf("Get max incorrect, have %d, was expecting %d ", max, item.c)
+	}
+	       
+	}
+}    
+   
+// FIbonacci Test          
+
+func TestFibo(t *testing.T)  {     
+
+    table :=[]struct{
+		n int  
+		r int 
+	}{
+		{1,1} ,  
+		{8,21},
+		{50,12586269025},      
+	}       
+   
+	for _, item := range table {   
+
+		    fibo :=Fibonacci(item.n)   
+
+			if fibo !=item.r{
+				t.Errorf("Fibo n  %d , was expecting %d ", fibo, item.r)
+			}	
+	}
+     
+}
+          
+
+
+
+
+
+
+
+
+
